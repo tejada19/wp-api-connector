baea47fd667aeef025696a7cb4f2d97e976a1d9f
tejada19 <65494988+tejada19@users.noreply.github.com>
2024-10-19 06:55:29 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,22 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Heroku WordPress API Connector is running');
+app.use(express.json());
+
+// Endpoint para procesar comandos
+app.post('/process-command', (req, res) => {
+    const command = req.body.entrada; // Obtener el comando desde el cuerpo de la solicitud
+    if (!command) {
+        return res.status(400).json({ error: 'Comando no proporcionado.' });
+    }
+
+    // Procesar el comando (lógica personalizada)
+    const resultado = `Comando procesado en Heroku: ${command}`;
+    
+    // Responder con el resultado procesado
+    res.json({ resultado });
 });
 
 app.listen(port, () => {
-  console.log(`App is running on port ${port}`);
+    console.log(`App is running on port ${port}`);
 });
